556792b411fb6f5dd1b22c5f5e7c3055bc822f17
jack <15700094730@163.com>
2022-11-07 17:25:38 +0800

    lc

--- 25.py
+++ 25.py
@@ -6,39 +6,43 @@
 
 
 class Solution:
-    # 翻转一个子链表，并且返回新的头与尾
-    def reverse(self, head: ListNode, tail: ListNode):
-        prev = tail.next
-        p = head
-        while prev != tail:
-            nex = p.next
-            p.next = prev
-            prev = p
-            p = nex
-        return tail, head
+    def reverseSon(self, head: ListNode, tail: ListNode):
+        pre = tail.next;
+        cur = head;
+        # 这里不能是cur != tail，因为pre在cur上一位，如果是cur的话，最终执行会少执行一次
+        while pre != tail:
+            next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        return tail, head;
 
     def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
-        hair = ListNode(0)
-        hair.next = head
-        pre = hair
-
+        headPre = ListNode(0)
+        # headPre.next指向了head，随着head的变化,head依旧是头部
+        headPre.next = head
+        # pre表示前一个子链的尾部节点
+        pre = headPre
         while head:
-            tail = pre
-            # 查看剩余部分长度是否大于等于 k
+            # sonTail必须从pre开始，如果从head开始的话，经过k次，那么会发现多走了一步
+            sonTail = pre
+            # 翻转子链
             for i in range(k):
-                tail = tail.next
-                if not tail:
-                    return hair.next
-            nex = tail.next
-            head, tail = self.reverse(head, tail)
-            # 把子链表重新接回原链表
+                sonTail = sonTail.next
+                if not sonTail:
+                    return headPre.next
+            nex = sonTail.next
+            # 翻转子链后，head为sonTail
+            head, sonTail = self.reverse(head, sonTail)
+            # 把子链与上个子链接上
             pre.next = head
-            tail.next = nex
-            pre = tail
-            head = tail.next
+            # 把子链与下个子链接上
+            sonTail.next = nex
+            # 重新赋值，开始下一轮
+            pre = sonTail
+            head = sonTail.next
 
-        return hair.next
-
+        return headPre.next
 
 
 
